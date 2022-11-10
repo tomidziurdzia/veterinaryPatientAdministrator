124b661eeb0b77dc0a300df6db74b8ec89d56f44
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-11-10 14:44:28 +1000

    Generando Token

--- backend/models/Veterinario.js
+++ backend/models/Veterinario.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import generarId from "../helpers/generarId.js";
 
 const veterinarioSchema = mongoose.Schema({
   nombre: {
@@ -27,6 +28,7 @@ const veterinarioSchema = mongoose.Schema({
   },
   token: {
     type: String,
+    default: generarId(),
   },
   confirmado: {
     type: Boolean,
