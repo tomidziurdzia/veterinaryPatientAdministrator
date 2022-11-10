eb319804a2a353485d6399204f76239d161a8737
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-11-10 14:38:43 +1000

    Previniendo usuarios duplicados

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,6 +1,15 @@
 import Veterinario from "../models/Veterinario.js";
 
 const registrar = async (req, res) => {
+  const { email } = req.body;
+  // Prevenir usuarios duplicados
+  const existeUsuario = await Veterinario.findOne({ email });
+
+  if (existeUsuario) {
+    const error = new Error("Usuario ya registrado");
+    return res.status(400).json({ msg: error.message });
+  }
+
   try {
     // Guardar un nuevo veterinario
     const veterinario = new Veterinario(req.body);
