25375b216fbe5563a5e70ed64b6dba1d2ff0b5ec
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-11-10 13:52:58 +1000

    Agregando variables de entorno

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import conectarDB from "./config/db.js";
 
 const app = express();
+dotenv.config();
+conectarDB();
 
 app.use("/", (req, res) => {
   res.send("Hola mundo");
 });
 
-app.listen(4000, () => {
-  console.log("Servidor funcionando en el puerto 4000");
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando en el puerto ${PORT}`);
 });
