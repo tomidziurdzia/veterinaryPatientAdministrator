ba65ad6e16743476e583752dfe9fd972bb6360f4
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-11-10 14:33:44 +1000

    Almacenando Registrs en la BD

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,9 +1,19 @@
-const registrar = (req, res) => {
-  res.send("Desde api");
+import Veterinario from "../models/Veterinario.js";
+
+const registrar = async (req, res) => {
+  try {
+    // Guardar un nuevo veterinario
+    const veterinario = new Veterinario(req.body);
+    const veterinarioGuardado = await veterinario.save();
+
+    res.json(veterinarioGuardado);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 const perfil = (req, res) => {
-  res.send("Desde perfil");
+  res.json({ msg: "Mostrando perfil" });
 };
 
 export { registrar, perfil };
