bf36962df19f47214f06ea0b4e192c641c3231ab
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-11-11 14:50:18 +1000

    Anadiendo rutas publicas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthLayout from "./layout/AuthLayout";
 import Login from "./paginas/Login";
+import Registrar from "./paginas/Registrar";
+import OlvidePassword from "./paginas/OlvidePassword";
+import ConfirmarCuenta from "./paginas/ConfirmarCuenta";
 
 const App = () => {
   return (
@@ -8,6 +11,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<AuthLayout />}>
           <Route index element={<Login />} />
+          <Route path="registrar" element={<Registrar />} />
+          <Route path="olvide-password" element={<OlvidePassword />} />
+          <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
         </Route>
       </Routes>
     </BrowserRouter>
