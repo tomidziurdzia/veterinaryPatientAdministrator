376546a72ba88f8b29fbfa16cb8d8f47bcbf077f
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-11-12 09:35:50 +1000

    Termine Estilos pagina olvide password

--- frontend/src/paginas/OlvidePassword.jsx
+++ frontend/src/paginas/OlvidePassword.jsx
@@ -1,5 +1,45 @@
+import { Link } from "react-router-dom";
+
 const OlvidePassword = () => {
-  return <div>OlvidePassword</div>;
+  return (
+    <>
+      <div className="md:h-24 h-24">
+        <h1 className="text-indigo-500 font-black px-5 text-2xl md:text-3xl text-center">
+          Recuperar Acceso
+        </h1>
+      </div>
+      <div className="md:w-3/5 mx-5 grid md:mx-auto shadow-lg py-5 md:py-10 px-5 rounded-xl bg-white">
+        <form action="">
+          <div className="mb-5">
+            <label className="text-gray-600 block text-xl font-bold" htmlFor="">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Email de registro"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+          </div>
+          <input
+            type="submit"
+            value="Recuperar"
+            className="bg-indigo-500 w-full md:w-auto mt-5 py-3 px-10 text-xl rounded-xl text-white font-bold hover:cursor-pointer hover:bg-indigo-600"
+          />
+        </form>
+        <nav className="mt-6 lg:flex lg:justify-between">
+          <Link className="block text-center my-5 text-gray-500" to="/">
+            Ya tienes una cuenta? Inicia Sesion
+          </Link>
+          <Link
+            className="block text-center my-5 text-gray-500"
+            to="/registrar"
+          >
+            No tienes una cuenta? Registrate
+          </Link>
+        </nav>
+      </div>
+    </>
+  );
 };
 
 export default OlvidePassword;
