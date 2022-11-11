020baa70ab7efe735481b4cdffbf7868cb79d1c1
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-11-11 11:59:49 +1000

    Termine Auth y recuperacion de contrasena

--- backend/routes/veterinarioRoutes.js
+++ backend/routes/veterinarioRoutes.js
@@ -4,15 +4,22 @@ import {
   perfil,
   confirmar,
   autenticar,
+  olvidePassword,
+  comprobarToken,
+  nuevoPassword,
 } from "../controllers/veterinarioController.js";
 import checkAuth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+// Area publica
 router.post("/", registrar);
 router.get("/confirmar/:token", confirmar);
 router.post("/login", autenticar);
+router.post("/olvide-password", olvidePassword);
+router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword);
 
+// Area privada
 router.get("/perfil", checkAuth, perfil);
 
 export default router;
